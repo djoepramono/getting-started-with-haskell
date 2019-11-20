f73ba83b96773599fe3dc0163a452f6bd2e7f9d3
Joe Pramono <djoepramono@gmail.com>
2019-11-20 14:06:43 +1100

    working play and play2 with comments

--- src/State.hs
+++ src/State.hs
@@ -5,36 +5,43 @@ import Control.Monad.State
 type GameState = Int
 initialState = 100
 
+-- State is a aggregtion of actions that can be applied onto an initial state point thing
+-- execState executes each action and get the final state value
+-- evalState exectues each action and get the final result 
+
 stateExample :: IO ()
 stateExample = do
-    print $ execState (play3 [1,2,0,5]) initialState
+    print $ execState (play2 [1,2,0,5]) initialState
 
 
--- play :: [Int] -> State GameState ()
--- play [] = do
---    stateValue <- get
---    return stateValue
--- play (head:tail) = do
---     stateValue <- get
---     put (stateValue + head)
---     play tail
-
--- play2 :: MonadState GameState m => [Int] -> m ()
--- play2 [] = do
---    stateValue <- get
---    return stateValue
--- play2 (head:tail) = do
---     stateValue <- get
---     put (stateValue + head)
---     play2 tail
-
-play3 :: (MonadState Int m, MonadIO m) => [Int] -> m ()
-play3 [] = do
-   stateValue <- get
-   _ <- liftIO $ putStrLn (show stateValue) 
-   put stateValue
-play3 (head:tail) = do
+play :: [Int] -> State GameState ()
+play [] = do
+    pure () -- at this point we have collected all actions, and thus dont do anything 
+play (head:tail) = do
     stateValue <- get
     put (stateValue + head)
-    _ <- liftIO $ putStrLn (show stateValue) 
-    play3 tail
+    play tail
+
+-- In this variant, we can use execState and evalState to get 108. It is unnecesary duplication
+-- It's currently not understood why the constraint is there
+-- play2 :: MonadState GameState m => [Int] -> m Int 
+play2 :: [Int] -> State GameState Int 
+play2 [] = do
+   stateValue <- get
+   pure stateValue
+play2 (head:tail) = do
+    stateValue <- get
+    put (stateValue + head)
+    play2 tail
+
+-- rewrite this!
+-- play3 :: (MonadState Int m, MonadIO m) => [Int] -> m ()
+-- play3 [] = do
+--    stateValue <- get
+--    _ <- liftIO $ putStrLn (show stateValue) 
+--    put stateValue
+-- play3 (head:tail) = do
+--     stateValue <- get
+--     put (stateValue + head)
+--    _ <- liftIO $ putStrLn (show stateValue) 
+--    play3 tail
